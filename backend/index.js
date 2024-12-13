fa7d2ab78a67eedb0566bb700100d6c3d3316258
deepak <deepakmehra7830@gmail.com>
2024-12-13 14:06:45 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+const app = express()
+dotenv.config()
+
+const port = process.env.PORT;
+const MONGO_URL = process.env.MONGO_URL
+console.log(MONGO_URL);
+
+
+app.get('/', (req, res)=>{
+    res.send('hello word')
+});
+
+app.listen(port, ()=>{
+    console.log(`server is runnig at port: ${port}`);
+    
+})
